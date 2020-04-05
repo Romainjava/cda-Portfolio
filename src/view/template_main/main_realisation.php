581555652e0a2e3d4a889de78ff6e0f09666994e
Romain <phenixstrato@gmail.com>
2020-04-05 15:09:55 +0200

    Travail du jour
    - Ajout du contenu dans Réalisation et du css avec
    Reste à faire :
    - Ajout et documentation du projet nestix
    - Voir pour ajouté d'autre projet ou links de travaux rendu.

--- src/view/template_main/main_realisation.php
+++ src/view/template_main/main_realisation.php
@@ -8,16 +8,24 @@
         </article>
 
         <article class="realisation__bloc target" data-id="2">
-            <img src="../public/assets/images/m_krav.png" alt="" class="realisation__bloc--img">
-            <h2 class="realisation__bloc--title">2</h2>
-            <span class="realisation__bloc--resume">Projet d'étude sur la réalisation d'un site vitrine...</span>
+            <img src="../public/assets/images/d_stephanie.png" alt="" class="realisation__bloc--img">
+            <h2 class="realisation__bloc--title">Consultante</h2>
+            <span class="realisation__bloc--resume">Projet pour une professionnelle indépendante...</span>
             <span class="realisation__bloc--btn">+</span>
         </article>
 
         <article class="realisation__bloc target" data-id="3">
-            <img src="../public/assets/images/m_krav.png" alt="" class="realisation__bloc--img">
-            <h2 class="realisation__bloc--title">3</h2>
-            <span class="realisation__bloc--resume">Projet d'étude sur la réalisation d'un site vitrine...</span>
+            <img src="../public/assets/images/d_nestix.png" alt="" class="realisation__bloc--img">
+            <h2 class="realisation__bloc--title">Projet nestix</h2>
+            <span class="realisation__bloc--resume">Front-end / Back-office / API en php, Client lourd et
+                application mobile en Java.</span>
+            <span class="realisation__bloc--btn">+</span>
+        </article>
+
+        <article class="realisation__bloc target" data-id="4">
+            <img src="../public/assets/images/d_playeraudio.png" alt="" class="realisation__bloc--img">
+            <h2 class="realisation__bloc--title">Player Audio</h2>
+            <span class="realisation__bloc--resume">Projet sur un player audio en JS</span>
             <span class="realisation__bloc--btn">+</span>
         </article>
     </div>
@@ -33,26 +41,27 @@
         </article>
 
         <article class="realisation__dekstop--article" data-id="2">
-            <img src="../public/assets/images/m_krav.png" alt="" class="realisation__dekstop--img">
+            <img src="../public/assets/images/d_stephanie.png" alt="" class="realisation__dekstop--img">
             <div class="realisation__dekstop--bloc">
-                <h2 class="realisation__dekstop--title">2</h2>
-                <span class="realisation__dekstop--resume">Projet d'étude sur la réalisation d'un site vitrine...</span>
+                <h2 class="realisation__dekstop--title">Consultante indépendante</h2>
+                <span class="realisation__dekstop--resume">Projet pour une professionnelle indépendante...</span>
             </div>
         </article>
 
         <article class="realisation__dekstop--article" data-id="3">
-            <img src="../public/assets/images/m_krav.png" alt="" class="realisation__dekstop--img">
+            <img src="../public/assets/images/d_nestix.png" alt="" class="realisation__dekstop--img">
             <div class="realisation__dekstop--bloc">
-                <h2 class="realisation__dekstop--title">3</h2>
-                <span class="realisation__dekstop--resume">Projet d'étude sur la réalisation d'un site vitrine...</span>
+                <h2 class="realisation__dekstop--title">Projet nestix</h2>
+                <span class="realisation__dekstop--resume">Front-end / Back-office / API en php, Client lourd et
+                application mobile en Java.</span>
             </div>
         </article>
 
         <article class="realisation__dekstop--article" data-id="4">
-            <img src="../public/assets/images/m_krav.png" alt="" class="realisation__dekstop--img">
+            <img src="../public/assets/images/d_playeraudio.png" alt="" class="realisation__dekstop--img">
             <div class="realisation__dekstop--bloc">
-                <h2 class="realisation__dekstop--title">3</h2>
-                <span class="realisation__dekstop--resume">Projet d'étude sur la réalisation d'un site vitrine...</span>
+                <h2 class="realisation__dekstop--title">Player Audio</h2>
+                <span class="realisation__dekstop--resume">Projet sur un player audio en JS</span>
             </div>
         </article>
 
@@ -61,6 +70,7 @@
         <span class="realisation__btn--1 btn active" data-target="1"></span>
         <span class="realisation__btn--2 btn" data-target="2"></span>
         <span class="realisation__btn--3 btn" data-target="3"></span>
+        <span class="realisation__btn--3 btn" data-target="4"></span>
     </div>
 
     <div class="realisation__modals">
@@ -78,7 +88,7 @@
             <span class="realisation__modals--resume">
                 Lien de la maquette établis :
                 <a href="https://www.figma.com/file/hi9wxZ0Bv44SJjStODWbl2/Krav-maga-Haim-zut"
-                target="_blank">
+                   target="_blank">
                     Maquette
                 </a>
             </span>
@@ -93,32 +103,58 @@
         </article>
 
         <article class="realisation__modals--article is-none" id="modal-2">
-            <img src="../public/assets/images/m_krav.png" alt="" class="realisation__modals--img">
-            <h2 class="realisation__bloc--title">2</h2>
-            <span class="realisation__modals--resume">Projet d'étude sur la réalisation d'un site vitrine...
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid autem consectetur,
-                cum impedit in minima optio perferendis quidem soluta.
-                A ad asperiores excepturi facere mollitia nobis provident repellendus tempora!</span>
+            <img src="../public/assets/images/d_stephanie.png" alt="" class="realisation__modals--img">
+            <h2 class="realisation__bloc--title">Consultante indépendante pour les établissements sociaux et médico
+                sociaux</h2>
+            <span class="realisation__modals--resume">Objectif : Développer un site front-end responsive pour
+                                                        une auto-entrepreneuse avec création de carte
+                                                        de visite ainsi que du logo de l'entreprise</span>
+            <span class="realisation__modals--resume">Introduction du site :
+            Dans le secteur depuis près de 15 ans, j'ai consacré toutes mes études au médico-social, du BEP Carrières Sanitaires
+                et Sociales au Master 2 Gestion des établissements de santé, parcours médico-social,
+                dont je suis diplômée depuis 2013. Une véritable passion m'anime pour ce secteur, que ce soit sur le terrain,
+                ou dans la réflexion stratégique.</span>
+            <span class="realisation__modals--resume">
+                Lien du site établis :
+                <a href="https://stephaniedavid-conseilesms.fr/"
+                   target="_blank">
+                    Site
+                </a>
+            </span>
             <span class="realisation__modals--btn">-</span>
         </article>
 
         <article class="realisation__modals--article is-none" id="modal-3">
-            <img src="../public/assets/images/m_krav.png" alt="" class="realisation__modals--img">
-            <h2 class="realisation__bloc--title">3</h2>
-            <span class="realisation__modals--resume">Projet d'étude sur la réalisation d'un site vitrine...
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid autem consectetur,
-                cum impedit in minima optio perferendis quidem soluta.
-                A ad asperiores excepturi facere mollitia nobis provident repellendus tempora!</span>
+            <img src="../public/assets/images/d_nestix.png" alt="" class="realisation__modals--img">
+            <h2 class="realisation__bloc--title">Projet nestix</h2>
+            <span class="realisation__modals--resume">à venir...</span>
             <span class="realisation__modals--btn">-</span>
         </article>
 
         <article class="realisation__modals--article is-none" id="modal-4">
-            <img src="../public/assets/images/m_krav.png" alt="" class="realisation__modals--img">
-            <h2 class="realisation__bloc--title">3</h2>
-            <span class="realisation__modals--resume">Projet d'étude sur la réalisation d'un site vitrine...
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid autem consectetur,
-                cum impedit in minima optio perferendis quidem soluta.
-                A ad asperiores excepturi facere mollitia nobis provident repellendus tempora!</span>
+            <img src="../public/assets/images/d_playeraudio.png" alt="" class="realisation__modals--img">
+            <h2 class="realisation__bloc--title">Player Audio</h2>
+            <span class="realisation__modals--resume">Objectif : réaliser en deux semaine un player audio qui sera
+                intégré au projet fil rouge.</span>
+            <span class="realisation__modals--resume">Pour cela, j'ai dû conceptualiser une idée graphique d'un player,
+                partant avec feuille blanche, rendre un premier MVP puis ensuite retravaillé et peaufiné l'idée...
+           S'en est suivi l'apprentissage de librairies, de faire du canvas,
+                de se créer une petite base de donnée en JSON, de faire du data-binding et templating en Javascript.</span>
+
+            <span class="realisation__modals--resume">
+                Lien de la maquette établis :
+                <a href="https://www.figma.com/file/lQX2HFmYtV6vd7HkZgbzfs/Player_audio?node-id=0%3A1"
+                   target="_blank">
+                    Maquette
+                </a>
+            </span>
+            <span class="realisation__modals--resume">
+                Lien du site établis :
+                <a href="https://vandamme.needemand.com/Player_Audio/"
+                   target="_blank">
+                    Player
+                </a>
+            </span>
             <span class="realisation__modals--btn">-</span>
         </article>
     </div>
